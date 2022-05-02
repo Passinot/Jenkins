3d5cdee4ae667112d53a4d29b9d52e3eb0bfdc6f
Passinot <101618434+Passinot@users.noreply.github.com>
2022-05-03 00:28:38 +0530

    Update Hellonihal.java

--- Hellonihal.java
+++ Hellonihal.java
@@ -2,4 +2,5 @@ class Hellonihal{
 public static void main(String[] args){
 System.out.println("This is nihal reporting to duty");
 System.out.println("Signing of");
+  System.out.println("bye");
 }}
